8a140aaca3bc108518342d8e332469c46c332b3d
blue0knight <oni_rivera98@yahoo.com>
2016-05-15 21:09:26 -0400

    added merchant paypal account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -26,6 +26,21 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def paypal_url(return_path)
+    values = {
+        business: "oni_rivera98-facilitator@yahoo.com",
+        cmd: "_xclick",
+        upload: 1,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
+        invoice: id,
+        amount: course.price,
+        item_name: course.name,
+        item_number: course.id,
+        quantity: '1'
+    }
+    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+
   protect_from_forgery except: [:hook]
   def hook
     params.permit! # Permit all Paypal input params
